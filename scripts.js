e87cc880a18cc05c785b8882f51181197437580b
vubuntu20 <selo@vubuntu20.localdomain>
2020-04-19 10:20:50 -0700

    Replaced const by var. Really this time

--- scripts.js
+++ scripts.js
@@ -1,26 +1,26 @@
 window.onload = function()
 {
-    var canvas;
-    var canvasWidth = 900
-    var canvasHeight = 600
-    var ctx; 
-    var delay = 100
-    var snayki
-    var blockSize = 30 
-    var pommli
-    var witdhInBlocks = canvasWidth/blockSize
-    var heightInBlocks = canvasHeight/blockSize
-    var score = 0
-    var timeout
-    var userName
-    //var snakeColor
+    const canvasWidth = 900
+    const canvasHeight = 600
+    const blockSize = 30 
+    const canvas = document.createElement('canvas')
+    const ctx  = canvas.getContext("2d")
+    let delay = 100
+    let snayki
+    let pommli
+    const witdhInBlocks = canvasWidth/blockSize
+    const heightInBlocks = canvasHeight/blockSize
+    const centreX = canvasWidth / 2
+    const centreY = canvasHeight / 2
+    let score
     
     init()
     
     function init()
     {
-       
-        canvas = document.createElement('canvas')
+        const snakeColor = askSnakeColor()
+        const appleColor = askAppleColor()
+        const userName = askName()
         canvas.width = canvasWidth
         canvas.height = canvasHeight
         canvas.style.border = "30px solid grey"
@@ -28,13 +28,9 @@ window.onload = function()
         canvas.style.display = "block"
         canvas.style.background = "#ddd"
         this.document.body.appendChild(canvas)
-        ctx = canvas.getContext("2d")
-
-        userName = askName()
-        snakeColor = askSnakeColor()
-        appleColor = askAppleColor()
         snayki = new Snake([[6,4],[5,4],[4,4]],"right",snakeColor)
-        pommli = new Apple([10,10],appleColor)
+        pommli  = new Apple([10,10],appleColor)
+        score = 0
         refreshCanvas()
     }
 
@@ -69,19 +65,218 @@ window.onload = function()
 
     }
 
+    function gameOver()
+    {
+        ctx.save()
+        ctx.font = "bold 70px sans-serif"
+        ctx.fillStyle = "gray"
+        ctx.textAlign = "center"
+        ctx.textBaseline = "middle"
+        ctx.strokeStyle = "dark"
+        ctx.lineWidth = 5
+        ctx.strokeText("Game Over",centreX,centreY - 180)
+        ctx.fillText("Game Over",centreX,centreY - 180)
+        ctx.font = "bold 30px sans-serif"
+        ctx.strokeText("Appuyer sur la touche espace pour rejouer",centreX,centreY - 100)
+        ctx.fillText("Appuyer sur la touche espace pour rejouer",centreX,centreY - 100)
+        ctx.restore()
+    }
+
+    function ricoumencer()
+    {
+        snayki = new Snake([[6,4],[5,4],[4,4]],"right",snakeColor)
+        pommli = new Apple([10,10])
+        score = 0
+        clearTimeout(timeout)
+        refreshCanvas()
+    }
+
+    function drawScore()
+    {
+        ctx.save()
+        ctx.font = "bold 200px sans-serif"
+        ctx.fillStyle = "gray"
+        ctx.textAlign = "center"
+        ctx.textBaseline = "middle"
+        ctx.fillText(score.toString(),centreX,centreY)
+        ctx.restore()
+    }
+
+    function drawBlock(ctx,position)
+    {
+        const x = position[0] * blockSize;
+        const y = position[1] * blockSize;
+        ctx.fillRect(x,y,blockSize,blockSize)
+    }
+
+    function Snake(body,direction,color)
+    {
+        this.body = body
+        this.direction = direction
+        this.ateApple = false
+        this.color = color
+        this.draw = function()
+        {
+            ctx.save()
+            ctx.fillStyle = this.color
+            for(let i = 0; i < this.body.length; i++)
+            {
+                drawBlock(ctx, this.body[i])
+            }
+            ctx.restore()
+        }
+
+        this.move = function()
+        {
+            const nextPosition = this.body[0].slice();
+            switch(this.direction)
+            {
+                case "left":
+                    nextPosition[0] -= 1;
+                    break;
+                    
+                case "right":
+                    nextPosition[0] += 1; 
+                    break;
+                        
+                case "up":
+                    nextPosition[1] -= 1; 
+                    break;
+                
+                case "down":
+                    nextPosition[1] += 1;
+                    break;
+                
+                default:
+                    throw("Invalid direction !!");
+            }
+            this.body.unshift(nextPosition)
+            if(!(this.ateApple))
+                this.body.pop()
+            else
+                this.ateApple = false
+        }
+
+        this.setDirection = function(newDirection)
+        {
+            let allowedDirections;
+            switch(this.direction)
+            {
+                case "left":
+                case "right":
+                    allowedDirections = ["up","down"];
+                    break;
+                    
+                case "up":
+                case "down":
+                    allowedDirections = ["left","right"];
+                    break;
+                
+                default:
+                    throw("Invalid direction !!")
+            }
+            if(allowedDirections.indexOf(newDirection) > -1)
+            {
+                this.direction = newDirection;
+            }
+        }
+
+        this.checkCollision = function()
+        {
+            let wallCollision = false
+            let snakeCollision = false
+            const head = this.body[0]
+            const bodyRest = this.body.slice(1)
+            const snakeX = head[0]
+            const snakeY = head[1]
+            const minX = 0
+            const minY = 0
+            const maxX = witdhInBlocks - 1
+            const maxY = heightInBlocks - 1
+            const isOutHorizontal = snakeX < minX || snakeX > maxX
+            const isOutVertical = snakeY < minY || snakeY > maxY
+
+            if(isOutHorizontal || isOutVertical)
+            {
+                wallCollision = true;
+            }
+
+            for(let i = 0; i < bodyRest.length; i ++)
+            {
+                if(snakeX === bodyRest[i][0] && snakeY === bodyRest[i][1])
+                {
+                    snakeCollision = true
+                }
+            }
+
+            return wallCollision || snakeCollision
+
+        }
+
+        this.isEatingApple = function(appleToEat)
+        {
+            const head = this.body[0]
+            if(head[0] === appleToEat.position[0] && head[1] === appleToEat.position[1])
+                return true
+            else
+                return false
+        }
+    }
+
+    function Apple(position,color)
+    {
+        this.position = position
+        this.color = color
+        this.draw = function()
+        {
+            const radius = blockSize/2
+            const x = this.position[0]*blockSize + radius
+            const y = this.position[1]*blockSize + radius
+            ctx.save()
+            ctx.fillStyle = this.color
+            ctx.beginPath()
+            ctx.arc(x,y,radius,0,Math.PI*2,true)
+            ctx.fill()
+            ctx.restore()
+        }
+
+        this.setNewPosition = function()
+        {
+            const newX = Math.round(Math.random() * (witdhInBlocks -1))
+            const newY = Math.round(Math.random() * (heightInBlocks -1))
+            this.position = [newX,newY]
+        }
+
+        this.isOnSnake = function(snakeToCheck)
+        {
+            let isOnSnake = false
+            for(let i = 0; i < snakeToCheck.body.length; i ++)
+            {
+                if(this.position[0] === snakeToCheck.body[i][0] &&  this.position[1] === snakeToCheck.body[i][1])
+                {
+                    isOnSnake = true;
+                }
+            }
+
+            return isOnSnake
+        }
+
+    }
+
     function askName(){
-        var userName = prompt("Quel est ton nom ? ")
+        userName = prompt("Quel est ton nom ? ")
         return userName
     }
 
     function askAppleColor()
     {
-        var colorChoice = prompt("Rendez-vous sur le site https://www.w3schools.com/colors/colors_picker.asp et coller le code hexadécimal (commence par #) de la couleur que vous souhaitez pour la pomme")
+        colorChoice = prompt("Rendez-vous sur le site https://www.w3schools.com/colors/colors_picker.asp et coller le code hexadécimal (commence par #) de la couleur que vous souhaitez pour la pomme")
         return colorChoice
     }
+
     function askSnakeColor()
     {
-        var colorChoice = prompt("Couleur du serpent ? Violet (v) / Rouge (r) / Jaune (j)")
+        const colorChoice = prompt("Couleur du serpent ? Violet (v) / Rouge (r) / Jaune (j)")
         console.log("touche pressee: " + colorChoice)
         switch (colorChoice){
 
@@ -114,211 +309,11 @@ window.onload = function()
         ctx.restore()
     }
 
-    function gameOver()
-    {
-        ctx.save()
-        ctx.font = "bold 70px sans-serif"
-        ctx.fillStyle = "gray"
-        ctx.textAlign = "center"
-        ctx.textBaseline = "middle"
-        ctx.strokeStyle = "dark"
-        ctx.lineWidth = 5
-        var centreX = canvasWidth / 2
-        var centreY = canvasHeight / 2
-        ctx.strokeText("Game Over",centreX,centreY - 180)
-        ctx.fillText("Game Over",centreX,centreY - 180)
-        ctx.font = "bold 30px sans-serif"
-        ctx.strokeText("Appuyer sur la touche espace pour rejouer",centreX,centreY - 100)
-        ctx.fillText("Appuyer sur la touche espace pour rejouer",centreX,centreY - 100)
-        ctx.restore()
-    }
-
-    function drawScore()
-    {
-        ctx.save()
-        ctx.font = "bold 200px sans-serif"
-        ctx.fillStyle = "gray"
-        ctx.textAlign = "center"
-        ctx.textBaseline = "middle"
-        var centreX = canvasWidth / 2
-        var centreY = canvasHeight / 2
-        ctx.fillText(score.toString(),centreX,centreY)
-        ctx.restore()
-    }
-
-    function drawBlock(ctx,position)
-    {
-        var x = position[0] * blockSize;
-        var y = position[1] * blockSize;
-        ctx.fillRect(x,y,blockSize,blockSize)
-    }
-
-    function Apple(position,color)
-    {
-        this.position = position
-        this.color = color
-        this.draw = function()
-        {
-            ctx.save()
-            ctx.fillStyle = this.color
-            ctx.beginPath()
-            var radius = blockSize/2
-            var x = this.position[0]*blockSize + radius
-            var y = this.position[1]*blockSize + radius
-            ctx.arc(x,y,radius,0,Math.PI*2,true)
-            ctx.fill()
-            ctx.restore()
-        }
-        this.setNewPosition = function()
-        {
-            var newX = Math.round(Math.random() * (witdhInBlocks -1))
-            var newY = Math.round(Math.random() * (heightInBlocks -1))
-            this.position = [newX,newY]
-        }
-
-        this.isOnSnake = function(snakeToCheck)
-        {
-            var isOnSnake = false
-            for(i = 0; i < snakeToCheck.body.length; i ++)
-            {
-                if(this.position[0] === snakeToCheck.body[i][0] &&  this.position[1] === snakeToCheck.body[i][1])
-                {
-                    isOnSnake = true;
-                }
-            }
-
-            return isOnSnake
-        }
-
-    }
-
-    function Snake(body,direction,color)
-    {
-        this.body = body
-        this.direction = direction
-        this.ateApple = false
-        this.color = color
-        this.draw = function()
-        {
-            ctx.save()
-            ctx.fillStyle = this.color
-            for(var i = 0; i < this.body.length; i++)
-            {
-                drawBlock(ctx, this.body[i])
-            }
-            ctx.restore()
-        }
-
-        this.move = function()
-        {
-            var nextPosition = this.body[0].slice();
-            switch(this.direction)
-            {
-                case "left":
-                    nextPosition[0] -= 1;
-                    break;
-                    
-                case "right":
-                    nextPosition[0] += 1; 
-                    break;
-                        
-                case "up":
-                    nextPosition[1] -= 1; 
-                    break;
-                
-                case "down":
-                    nextPosition[1] += 1;
-                    break;
-                
-                default:
-                    throw("Invalid direction !!");
-            }
-            this.body.unshift(nextPosition)
-            if(!(this.ateApple))
-                this.body.pop()
-            else
-                this.ateApple = false
-        }
-
-        this.setDirection = function(newDirection)
-        {
-            var allowedDirections;
-            switch(this.direction)
-            {
-                case "left":
-                case "right":
-                    allowedDirections = ["up","down"];
-                    break;
-                    
-                case "up":
-                case "down":
-                    allowedDirections = ["left","right"];
-                    break;
-                
-                default:
-                    throw("Invalid direction !!")
-            }
-            if(allowedDirections.indexOf(newDirection) > -1)
-            {
-                this.direction = newDirection;
-            }
-        }
-
-        this.checkCollision = function()
-        {
-            var wallCollision = false
-            var snakeCollision = false
-            var head = this.body[0]
-            var bodyRest = this.body.slice(1)
-            var snakeX = head[0]
-            var snakeY = head[1]
-            var minX = 0
-            var minY = 0
-            var maxX = witdhInBlocks - 1
-            var maxY = heightInBlocks - 1
-            var isOutHorizontal = snakeX < minX || snakeX > maxX
-            var isOutVertical = snakeY < minY || snakeY > maxY
-
-            if(isOutHorizontal || isOutVertical)
-            {
-                wallCollision = true;
-            }
-
-            for(var i = 0; i < bodyRest.length; i ++)
-            {
-                if(snakeX === bodyRest[i][0] && snakeY === bodyRest[i][1])
-                {
-                    snakeCollision = true
-                }
-            }
-
-            return wallCollision || snakeCollision
-
-        }
-
-        this.isEatingApple = function(appleToEat)
-        {
-            var head = this.body[0]
-            if(head[0] === appleToEat.position[0] && head[1] === appleToEat.position[1])
-                return true
-            else
-                return false
-        }
-    }
-
-    function ricoumencer()
-    {
-        snayki = new Snake([[6,4],[5,4],[4,4]],"right",snakeColor)
-        pommli = new Apple([10,10])
-        score = 0
-        clearTimeout(timeout)
-        refreshCanvas()
-    }
     
 document.onkeydown = function handleKeyDown(e)
 {
-    var key = e.keyCode;
-    var newDirection;
+    const key = e.keyCode;
+    let newDirection;
 
     switch(key)
     {
